2f6c0fc0f9cf87463aa1d9a3b687fec845f04bef
pinnouse <cupofpho@gmail.com>
2018-12-09 11:12:30 -0500

    Add sponsor ranks

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 'use strict';
 
-$(() => {
-  $(".page").each((ind, target) => {
-    var $target = $(target)
-    $target.prepend(`<div class="tooltip" data-page="${$target.attr("data-page")}"></div>`)
-  })
-})
+$(function() {
+  for (let i = 0; i < 150; i++) {
+    $('.view-container').append('<img src="Star.svg" class="star" style="left:'  + (Math.random() * 100) + 'vw;top: ' + (Math.random() * 100) + 'vh;">');
+  }
+
+  $(".page").each(function(ind, target) {
+    var $target = $(target);
+    $target.prepend('<div class="planet"><span class="cover"></span></div>').append('<div class="tooltip" data-page="' + $target.attr("data-page").toUpperCase() + '"></div>');
+    $target.click(function(ev) {
+      if ($(ev.target).hasClass("close")) return;
+      $target.addClass("selected");
+    });
+    $target.find(".container").append('<span class="close"></span>');
+  });
+
+  $(".close").click(function() {
+    var $selected = $(".selected");
+    $selected.find(".container").animate({
+      top: "100vh"
+    }, 380, function() {
+      $selected.removeClass("selected");
+      $selected.find(".container").removeAttr("style");
+    });
+  });
+});
